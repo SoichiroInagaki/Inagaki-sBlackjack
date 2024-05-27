c0eae8be2aef5929f75943774125bab07b6b2139
5593501507 <soichiro.inagaki@excite-software.co.jp>
2024-05-27 10:57:47 +0900

    カードクラスの定義づけ

--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -1,5 +1,41 @@
 package model;
 
 public class Card {
-
+	
+	String mark;
+	int number;
+	
+	public Card(String mark, int number) {
+		this.mark = mark;
+		this.number = number;
+	}
+	
+	public String getMark() {
+		return mark;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+	
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	public String getCard() {
+		switch(getNumber()) {
+			case 11:
+				return (getMark() + "J");
+			case 12:
+				return (getMark() + "Q");
+			case 13:
+				return (getMark() + "K");
+			default:
+				return (getMark() + Integer.toString(number));
+		}
+	}
 }
