06c33bdb15ce86bad1add59a886983a1a57d3a90
5593501507 <soichiro.inagaki@excite-software.co.jp>
2024-05-28 09:50:49 +0900

    ディーラーのヒット回数を属性として追加するように変更

--- src/main/java/controller/GameServlet.java
+++ src/main/java/controller/GameServlet.java
@@ -59,19 +59,24 @@ public class GameServlet extends HttpServlet {
 				nextPage = "win-end.jsp";
 				message = "ディーラーはバーストしました！";
 				request.setAttribute("message", message);
+				request.setAttribute("countHit", dealer.countHit());
 			}else {
 				if(dealer.getPoint() < playerInGame.getPoint()) {
 					if(playerInGame.getPoint() == 21) {
 						nextPage = "win-end.jsp";
 						message = "BLACKJACK!!";
 						request.setAttribute("message", message);
+						request.setAttribute("countHit", dealer.countHit());
 					}else {
 						nextPage = "win-end.jsp";
+						request.setAttribute("countHit", dealer.countHit());
 					}
 				}else if(dealer.getPoint() == playerInGame.getPoint()) {
 					nextPage = "draw-end.jsp";
+					request.setAttribute("countHit", dealer.countHit());
 				}else {
 					nextPage = "lose-end.jsp";
+					request.setAttribute("countHit", dealer.countHit());
 				}
 			}
 		}
