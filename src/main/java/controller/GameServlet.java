f0904eb6173c2e59bfdd66f09e4cda90dee43f5e
5593501507 <soichiro.inagaki@excite-software.co.jp>
2024-05-28 09:24:04 +0900

    ゲーム進行メソッドを追加

--- src/main/java/controller/GameServlet.java
+++ src/main/java/controller/GameServlet.java
@@ -27,19 +27,54 @@ public class GameServlet extends HttpServlet {
 		dealer.prepareHand(deck);
 		
 		HttpSession session = request.getSession();
+		session.setAttribute("deck", deck);
 		session.setAttribute("playerInGame", playerInGame);
 		session.setAttribute("dealer", dealer);
 		
-		request.getRequestDispatcher("InGame.jsp");
+		request.getRequestDispatcher("InGame.jsp").forward(request, response);;
 	}
 	
 	//ゲーム進行中に用いるメソッド
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String nextpage = null;
+		String nextPage = null;
+		String message = null;
+		HttpSession session = request.getSession();
+		Dealer dealer = (Dealer) session.getAttribute("dealer");
+		PlayerInGame playerInGame = (PlayerInGame) 
+				session.getAttribute("playerInGame");
+		Deck deck = (Deck) session.getAttribute("deck");
 		
 		if(request.getParameter("clicked").equals("hit")) {
-			
+			playerInGame.hit(deck);
+			if(playerInGame.confirmBurst()) {
+				nextPage = "burst-end.jsp";
+			}else {
+				nextPage = "in-game.jsp";
+				message = "ヒットしました";
+				request.setAttribute("message", message);
+			}
+		}else {
+			dealer.hit(deck);
+			if(dealer.confirmBurst()) {
+				nextPage = "win-end.jsp";
+				message = "ディーラーはバーストしました！";
+				request.setAttribute("message", message);
+			}else {
+				if(dealer.getPoint() < playerInGame.getPoint()) {
+					if(playerInGame.getPoint() == 21) {
+						nextPage = "win-End";
+						message = "BLACKJACK!!";
+						request.setAttribute("message", message);
+					}else {
+						nextPage = "win-end";
+					}
+				}else if(dealer.getPoint() == playerInGame.getPoint()) {
+					nextPage = "draw-end";
+				}else {
+					nextPage = "lose-end";
+				}
+			}
 		}
-		
+		request.getRequestDispatcher(nextPage).forward(request, response);
 	}
 }
