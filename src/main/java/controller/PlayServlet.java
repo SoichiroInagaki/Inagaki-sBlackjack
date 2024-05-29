58c00dc2030e213b8883b252bd0bea7a41f48ec4
5593501507 <soichiro.inagaki@excite-software.co.jp>
2024-05-29 15:42:03 +0900

    現在保有中のチップを表示するためのサーブレットを作成・定義

---
+++ src/main/java/controller/PlayServlet.java
@@ -0,0 +1,48 @@
+package controller;
+
+import java.io.IOException;
+
+import dao.PlayerDao;
+import exception.BlackjackException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.Player;
+
+@WebServlet("/PlayServlet")
+public class PlayServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		Player player = (Player) session.getAttribute("player");
+		int chip = 0;
+		String nextPage = null;
+		String message = null;
+		
+		try {
+			PlayerDao playerDao = new PlayerDao();
+			chip = playerDao.getChip(player.getId());
+			nextPage = "play.jsp";
+		}catch(BlackjackException e) {
+			message = e.getMessage();
+			request.setAttribute("message", message);
+			nextPage = "menu.jsp";
+		}
+		
+		request.setAttribute("chip", chip);
+		request.getRequestDispatcher(nextPage).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
