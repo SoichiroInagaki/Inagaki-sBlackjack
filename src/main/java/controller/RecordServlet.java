e95b73a5739d4ec19c338219338e24010ba5f39f
5593501507 <soichiro.inagaki@excite-software.co.jp>
2024-05-29 09:49:50 +0900

    戦績をリクエストに属性付与

--- src/main/java/controller/RecordServlet.java
+++ src/main/java/controller/RecordServlet.java
@@ -1,37 +1,43 @@
 package controller;
 
+import java.io.IOException;
+
+import dao.PlayerDao;
+import exception.BlackjackException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import jakarta.servlet.http.HttpSession;
+import model.Player;
 
-/**
- * Servlet implementation class RecordServlet
- */
+@WebServlet("/RecordServlet")
 public class RecordServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RecordServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
+	//MenuServletのdoGetメソッドから連動して動作
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		HttpSession session = request.getSession();
+		Player loginedPlayer = (Player) session.getAttribute("player");
+		Player playerForRecord = null;
+		Player[] rankedRecords = null;
+		
+		try {
+			PlayerDao playerDao = new PlayerDao();
+			playerForRecord = playerDao.getRecord(loginedPlayer);
+			rankedRecords = playerDao.getRankedRecords();
+			
+			request.setAttribute("playerForRecord", playerForRecord);
+			request.setAttribute("rankedRecords", rankedRecords);
+		}catch(BlackjackException e) {
+			String message = e.getMessage();
+			request.setAttribute("message", message);
+		}
+		request.getRequestDispatcher("record.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
